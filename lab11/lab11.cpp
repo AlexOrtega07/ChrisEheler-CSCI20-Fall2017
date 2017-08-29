06893626e9d688dc2aea0ac0ad808763d57cd498
Chris Eheler <ceheler001@student.butte.edu>
2017-08-29 03:40:41 +0000

    lab11 commit

---
+++ lab11/lab11.cpp
@@ -0,0 +1,12 @@
+computer generates random number
+this number is now "the answer"
+computer asks user for a number
+the users number is now "guess"
+computer checks to see if "guess" is "the answer"
+if it is than the user will be awarded 10 points and will display "You Win!!"
+if not than the computer will determine if "guess" is higher or lower "than answer"
+the computer will now show the user the word "higher" or "lower" depending upon which is true
+repeat steps 3 - 8 
+the computer will keep track of the number itterations that step 9 is performed
+that number is "wrong answers"
+computer will award 10 - "wrong answers" and award than number of points to the user and restart the process
