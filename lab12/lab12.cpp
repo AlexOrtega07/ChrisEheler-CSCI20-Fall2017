a5e7e2124cf45df3811f34bb73c4c37175156e59
Chris Eheler <ceheler001@student.butte.edu>
2017-08-30 01:28:34 +0000

    lab12 commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,21 @@
+// Created by: Chris Eheler
+// Created on: 8/28/2017
+
+
+ #include <iostream>                       
+ using namespace std;                    
+
+ int main()                             
+ {                                       
+     cout<<"  _                             "<<endl;      
+     cout<<" ( \\                           "<<endl;
+     cout<<"  ) )                           "<<endl;
+     cout<<" ( (  .-'-.  A.-.A              "<<endl;
+     cout<<"  \\ \\/     \\/ , , \\         "<<endl;
+     cout<<"   \\ \\    = ;  t  /=          "<<endl;
+     cout<<"    \\   |''-  ',--'            "<<endl;
+     cout<<"     / //  | ||                 "<<endl;
+     cout<<"    /_,))  |_,))                "<<endl;
+     
+ }                                      
+ 
