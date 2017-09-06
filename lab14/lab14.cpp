ffe17e40b54cb7792d73d624f0cac4f7746fbcce
Chris Eheler <ceheler001@student.butte.edu>
2017-09-06 02:44:18 +0000

    lab 1.4 submission

--- lab14/lab14.cpp
+++ lab14/lab14.cpp
@@ -8,37 +8,38 @@
 
  int main()                             
  {          
-            int change_input = 0;                                                   //Establishing placeholders for output ands input variables
+            int change_input = 0;                                                                                              //Establishing placeholders for output ands input variables
             int currency_out = 0;
             int quarters_out = 0;
             int dimes_out = 0;
             int nickles_out = 0;
             int pennies_out = 0;
-            int quarter = 25;                                                        //Establishing currency value relative to input
+            int quarter = 25;                                                                                                    //Establishing currency value relative to input
             int dime = 10;
             int nickle = 5;
             int penny = 1;
-            int total_without_quarters = 0;                                          //Establish calcuation variables 
+            int total_without_quarters = 0;                                                                                      //Establish calcuation variables 
             int total_without_quarters_and_dimes = 0;
             int total_with_only_pennys = 0;
             float total_payout = 0.0;
             
-            cout << "Please input the currency amount deposited in cents:";          //How much change was put in the machine
+            cout << "Please input the currency amount deposited in cents:";                                                       //How much change was put in the machine
             cin >> change_input;
-            cout << "You have entered " << change_input << " cents." << endl;        //Tells the user how much change they put in
+            cout << "You have entered " << change_input << " cents." << endl;                                                     //Tells the user how much change they put in
             
             quarters_out = change_input / quarter;
             total_without_quarters = change_input - (quarters_out * quarter);
             dimes_out = total_without_quarters / dime;
             total_without_quarters_and_dimes = change_input - (quarters_out * quarter) - (dimes_out * dime);
             nickles_out = total_without_quarters_and_dimes / nickle;
-            total_with_only_pennys = total_without_quarters_and_dimes % nickle;      //Establish how many of each coin type was put in
+            total_with_only_pennys = total_without_quarters_and_dimes % nickle;                                                   //Establish how many of each coin type was put in
             
-            cout << "You have entered " << quarters_out << " quarters, " << dimes_out << " dimes " << nickles_out << " nickles, " << total_with_only_pennys << " pennies" << endl;
+            cout << "You have entered " << quarters_out << " quarters, " << dimes_out;
+            cout << " dimes " << nickles_out << " nickles, " << total_with_only_pennys << " pennies" << endl;
             
-            total_payout = (change_input - (change_input * .109)) * .01 ;            //Take your fee
+            total_payout = (change_input - (change_input * .109)) * .01 ;                                                         //Take your fee
              
-            cout <<"The total is $" << change_input / 100.0 << " After the fee you get $" << total_payout << endl;
+            cout <<"The total is $" << change_input / 100.0 << " After the fee you get $" << total_payout << endl;                //Output the total
             
             
  }
