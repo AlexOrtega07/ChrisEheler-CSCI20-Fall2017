22414637a9ab1dd5dac16ce50a94e0f92d448120
Chris Eheler <ceheler001@student.butte.edu>
2017-09-27 02:18:28 +0000

    Lab2.3 submission

--- lab23/lab23.cpp
+++ lab23/lab23.cpp
@@ -1,13 +1,17 @@
 // Created By Chris Eheler
 // Created on 09/26/2017
-// Program is going to generate a random number and then print it while accepting values to pick between and allowing swaping of numbers
+// Program is going to generate a random number in an overly complex and confusing manner
 
 #include <iostream>
 #include <cstdlib>                                                                               //Including the libraries required
 #include <ctime>
 using namespace std;
 
-void swap(int& min_random, int& max_random)
+/*
+This functions will take the minimum value and maximum value and swap them using call by function to change it in memory
+*/
+
+void swap(int& min_random, int& max_random)                                                     
 {
     int place_holder=0;
     place_holder = min_random;
@@ -16,23 +20,27 @@ void swap(int& min_random, int& max_random)
     
 }
 
-void rand_generator(int min_random, int max_random)                                               //Defining the function
+/*
+This function calls a random number between two numbers later inputed by the user
+*/
+
+void rand_generator(int min_random, int max_random)                                               
 {
     swap(min_random, max_random);
     srand(time(0));                                                                               //Seed in time
     int generated_number = rand() % ( max_random - min_random + 1 ) + max_random ;                //Generating a random number 
     cout << "Your random number is " << generated_number << endl;
     
-                                                                                                  //Printing the generated number
+                                                                                              
 }
 
 
 
-int main()
+int main()                                                                                        //Running the function
 {
     int minimum_rand;
     int maximum_rand;
-                                                                                                  //Running the function
+                                                                                                 
     cout << "Please input you random number minimum value: " << endl;
     cin >> minimum_rand;
     cout << "Please input your random number maximum value: " << endl;
