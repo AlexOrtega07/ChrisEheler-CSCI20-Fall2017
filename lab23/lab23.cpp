626df4c561bc8fe2b227e3a021a0c82fc9ca0a91
Chris Eheler <ceheler001@student.butte.edu>
2017-09-29 17:01:59 +0000

    Lab2.3 Submission

--- lab23/lab23.cpp
+++ lab23/lab23.cpp
@@ -1,5 +1,5 @@
 // Created By Chris Eheler
-// Created on 09/26/2017
+// Created on 09/26/2017 Most recently Edited on: 09/29/2017
 // Program is going to generate a random number in an overly complex and confusing manner using a swap function switching input values
 
 #include <iostream>
@@ -9,6 +9,7 @@ using namespace std;
 
 /*
 This functions will take the minimum value and maximum value and swap them using call by function to change it in memory
+doing so by taking the two inputs and switching them and using a place holder variable for the swap process to hold a the min value temporarily
 */
 
 void Swap(int& min_random, int& max_random)                                                     
@@ -21,7 +22,8 @@ void Swap(int& min_random, int& max_random)
 }
 
 /*
-This function calls a random number between two numbers later inputed by the user and seeds in time to make the number less predictable
+This function calls a random number between two numbers and seeds in time to make the number less predictable
+taking two user inputs and using a math equation using modulo to determine the usable range of numbers
 */
 
 void RandGenerator(int min_random, int max_random)                                               
@@ -47,7 +49,7 @@ int main()                                                                      
     cout << "Please input your random number maximum value: " << endl;
     cin >> maximum_rand;
     
-    RandGenerator(minimum_rand, maximum_rand);
+    RandGenerator(minimum_rand, maximum_rand);                                                    //Call the RandGenerator function to use the user input to now generate a random numbers between the users numbers
     
     return 0;
 }
