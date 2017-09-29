bf115bda0b65af10ec25d0fff040e8844e28f80e
Chris Eheler <ceheler001@student.butte.edu>
2017-09-29 16:58:15 +0000

    Lab2.3 Submission

--- lab23/lab23.cpp
+++ lab23/lab23.cpp
@@ -1,6 +1,6 @@
 // Created By Chris Eheler
 // Created on 09/26/2017
-// Program is going to generate a random number in an overly complex and confusing manner
+// Program is going to generate a random number in an overly complex and confusing manner using a swap function switching input values
 
 #include <iostream>
 #include <cstdlib>                                                                               //Including the libraries required
@@ -11,7 +11,7 @@ using namespace std;
 This functions will take the minimum value and maximum value and swap them using call by function to change it in memory
 */
 
-void swap(int& min_random, int& max_random)                                                     
+void Swap(int& min_random, int& max_random)                                                     
 {
     int place_holder=0;
     place_holder = min_random;
@@ -21,14 +21,15 @@ void swap(int& min_random, int& max_random)
 }
 
 /*
-This function calls a random number between two numbers later inputed by the user
+This function calls a random number between two numbers later inputed by the user and seeds in time to make the number less predictable
 */
 
-void rand_generator(int min_random, int max_random)                                               
+void RandGenerator(int min_random, int max_random)                                               
 {
-    swap(min_random, max_random);
+    int generated_number = 0;
+    Swap(min_random, max_random);
     srand(time(0));                                                                               //Seed in time
-    int generated_number = rand() % ( max_random - min_random + 1 ) + max_random ;                //Generating a random number 
+    generated_number = rand() % ( max_random - min_random + 1 ) + max_random ;                //Generating a random number 
     cout << "Your random number is " << generated_number << endl;
     
                                                                                               
@@ -36,17 +37,17 @@ void rand_generator(int min_random, int max_random)
 
 
 
-int main()                                                                                        //Running the function
+int main()                                                                                        //Running the main function
 {
-    int minimum_rand;
-    int maximum_rand;
+    int minimum_rand = 0;                                                                         //Initializing values
+    int maximum_rand = 0;
                                                                                                  
     cout << "Please input you random number minimum value: " << endl;
     cin >> minimum_rand;
     cout << "Please input your random number maximum value: " << endl;
     cin >> maximum_rand;
     
-    rand_generator(minimum_rand, maximum_rand);
+    RandGenerator(minimum_rand, maximum_rand);
     
     return 0;
 }
