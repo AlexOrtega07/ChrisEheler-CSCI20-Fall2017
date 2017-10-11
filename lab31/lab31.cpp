bc81ee6ba2f45afd0570c93684697d8f75078675
Chris Eheler <ceheler001@student.butte.edu>
2017-10-11 06:55:42 +0000

    Lab 3.1 Submission

--- lab31/lab31.cpp
+++ lab31/lab31.cpp
@@ -11,6 +11,9 @@ int main(){
     int num_data = 0;
     int num_tablets = 0;
     int ATT_UCP_total = 0;
+    int ATT_FP_total = 0;
+    int verizon_unl_total = 0;
+    int sprint_unl_total = 0;
     
     cout << "Please enter the total number of phones in the plan: " << endl;
     cin >> num_phones;
@@ -30,6 +33,42 @@ int main(){
     else{
         ATT_UCP_total = 115 + (((num_phones - 2) * 20) + (num_tablets * 10));
     }
+    
+    if (num_data == 1) {
+        ATT_FP_total = (num_phones * 20) + (num_tablets *10) + 30;
+    }
+    
+    else if (num_data <= 3){
+        ATT_FP_total = (num_phones * 20) + (num_tablets *10) + 40;
+    }
+    
+    else if (num_data <= 6){
+        ATT_FP_total = (num_phones * 20) + (num_tablets *10) + 60;
+    }    
+    
+    else if (num_data <= 10){
+        ATT_FP_total = (num_phones * 20) + (num_tablets *10) + 80;
+    }
+    
+    else if (num_data <= 16){
+        ATT_FP_total = (num_phones * 20) + (num_tablets *10) + 90;
+    }
+    
+    else {
+        ATT_FP_total = (num_phones * 20) + (num_tablets *10) + 110;
+    }
+    
+    if (num_phones == 1){
+        verizon_unl_total = 80 + (num_tablets * 10);
+    }
+    
+     else {
+        verizon_unl_total = 110 + ( (num_phones - 1) * 20 ) + (num_tablets * 10);
+    }
+    
+    
+    
+    cout << ATT_UCP_total << "  " << ATT_FP_total;
     
     return(0);
 }
