3121cb737d4ce67c76e1c6341751e5fa197d9bb2
Chris Eheler <ceheler001@student.butte.edu>
2017-10-25 04:45:44 +0000

    Lab 3.5 Submission

--- lab35/lab35.cpp
+++ lab35/lab35.cpp
@@ -6,10 +6,10 @@
 #include<iostream>
 using namespace std;
 
-class TemperatureConverter{
+class TemperatureConverter{                                                     // Create Class 
     
     public:
-    void SetTempFromKelvin( double kelvin_);
+    void SetTempFromKelvin( double kelvin_);                                    // Defining accesors and mutators
     void SetTempFromCelsius(double celsius_);
     void SetTempFromFahrenheit(double farenheit_);
     void PrintTemperatures() const;
@@ -21,17 +21,17 @@ class TemperatureConverter{
     TemperatureConverter(double kelvin_in);
     
     private:
-    double kelvin_;
+    double kelvin_;                                                             // One and only variable
     
 };
 
-TemperatureConverter::TemperatureConverter()
+TemperatureConverter::TemperatureConverter()                                    // Default constructor
 {
     kelvin_ = 0.0;
     return;
 }
 
-TemperatureConverter::TemperatureConverter(double kelvin_in)
+TemperatureConverter::TemperatureConverter(double kelvin_in)                    // Overload constructor to take an input for kelvin temp
 {
     if (kelvin_in >= 0){
     kelvin_ = kelvin_in;
@@ -42,7 +42,12 @@ TemperatureConverter::TemperatureConverter(double kelvin_in)
     return;
 }
 
-void TemperatureConverter::SetTempFromKelvin(double kelvin_in){
+/*
+The foillowiong functions are accsors and mutators who exist only to allow the program to access the private variable of "double kelvin_"
+the gets need a type of double as they are returning a double where the accessors are not returning a value that is going to be used and are a void type
+*/
+
+void TemperatureConverter::SetTempFromKelvin(double kelvin_in){                 // Defining functions for the class accesors and mutators
     kelvin_ = kelvin_in;
     return;
 }
@@ -78,17 +83,17 @@ void TemperatureConverter::PrintTemperatures() const {
 
 int main ()
 {
-    TemperatureConverter temp1; //testing default constructor
-    TemperatureConverter temp2(274); //testing overloaded constructor
+    TemperatureConverter temp1;                                                  //testing default constructor
+    TemperatureConverter temp2(274);                                             //testing overloaded constructor
     
     temp1.PrintTemperatures();
     temp2.PrintTemperatures();
     
-    temp1.SetTempFromKelvin(400.15); //testing mutator function
-    cout<<temp1.GetTempFromKelvin()<<endl;//testing accessor function
+    temp1.SetTempFromKelvin(400.15);                                            //testing mutator function
+    cout<<temp1.GetTempFromKelvin()<<endl;                                      //testing accessor function
     temp1.PrintTemperatures();
     
-    temp2.SetTempFromCelsius(32); //testing other functions
+    temp2.SetTempFromCelsius(32);                                                //testing other functions
     cout<<temp2.GetTempAsCelsius()<<endl;
     temp2.PrintTemperatures();
     
@@ -98,3 +103,28 @@ int main ()
     
     return 0;
 }
+
+/*
+Running /home/ubuntu/workspace/lab35/lab35.cpp
+The temperature in kelvin is: 0
+The temperature in Celsius is: -273.15
+The temperature in Farenheiet is: -459.67
+The temperature in kelvin is: 274
+The temperature in Celsius is: 0.85
+The temperature in Farenheiet is: 33.53
+400.15
+The temperature in kelvin is: 400.15
+The temperature in Celsius is: 127
+The temperature in Farenheiet is: 260.6
+32
+The temperature in kelvin is: 305.15
+The temperature in Celsius is: 32
+The temperature in Farenheiet is: 89.6
+32
+The temperature in kelvin is: 273.15
+The temperature in Celsius is: 0
+The temperature in Farenheiet is: 32
+
+
+Process exited with code: 0
+*/
