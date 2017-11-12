3d2d7f5ff14f6baa8f8d41da288fe81adf34f554
Chris Eheler <ceheler001@student.butte.edu>
2017-11-12 07:34:49 +0000

    Lab 4.4 Submission

--- lab44/lab44.cpp
+++ lab44/lab44.cpp
@@ -1,34 +1,31 @@
-//
-//
-//
+//Created by: Chris Eheler
+//Created on: 10/10/2017
+//This Program takes in a users first and last names and generates suggested user names for them to use
 
 #include <iostream>
-#include <cstring>
-#include <cctype>
+#include <cstring>                                                              // Needed to do CString operations
 using namespace std;
 
 int main(){
     
-    char first_name [10] = "";
+    char first_name [10] = "";                                                  // Establishing first and last names of the user with max of 10 letters for hte first name and 20 for the last
     char last_name [20] = "";
-    char second_suggestion [30] = "";
-    char third_suggestion [30] = "";
     
-    cout << "Please enter your first name (Up to 10 Letters): ";
+    cout << "Please enter your first name (Up to 10 Letters): ";                // gathering user input for first name
     cin >> first_name;
     
-    while (strlen(first_name) > 10){
+    while (strlen(first_name) > 10){                                            // Makes sure the name is within the specified amount of chars
         cout << "Your first name is too long try a nickname: ";
         cin >> first_name;
     }
-    cout << "Please enter your last name (Up to 20 Letters): ";
+    cout << "Please enter your last name (Up to 20 Letters): ";                 // Gets last name input
     cin >> last_name ;
     while (strlen(last_name) > 20){
-        cout << "Your last name is too long try a shortened version: ";
+        cout << "Your last name is too long try a shortened version: ";         // makes sure the length is correct
         cin >> last_name;
     }
     
-    if (strcmp(first_name, last_name) == 0) {  
+    if (strcmp(first_name, last_name) == 0) {                                   // If the two names are the same it wans the user and gives another chance in case their names are actually the same
         cout << "Your last name and first name are the same, please restart the program if this is an error." << endl;
         cout << "Please enter your first name (Up to 10 Letters): ";
         cin >> first_name;
@@ -37,8 +34,8 @@ int main(){
         
     }
     cout << endl;
-    cout << "Which of the followuing user names do you like?" << endl << endl;
-    cout << first_name << " " << last_name << endl;
+    cout << "Which of the followuing user names do you like?" << endl << endl;  // Offers choices of first then last then using initials as well as the last name
+    cout << first_name << last_name << endl;
     cout << first_name[0] << last_name << endl;
     cout << last_name << first_name[0];
     return(0);
@@ -50,7 +47,7 @@ Please enter your last name (Up to 20 Letters): Eheler
 
 Which of the followuing user names do you like?
 
-Chris Eheler
+ChrisEheler
 CEheler
 EhelerC
 
@@ -61,7 +58,7 @@ Please enter your last name (Up to 20 Letters): Browne
 
 Which of the followuing user names do you like?
 
-April Browne
+AprilBrowne
 ABrowne
 BrowneA
 
@@ -73,11 +70,24 @@ Please enter your last name (Up to 20 Letters): Hazeltine-Smith
 
 Which of the followuing user names do you like?
 
-Ana Hazeltine-Smith
+AnaHazeltine-Smith
 AHazeltine-Smith
 Hazeltine-SmithA
 
 --------------------------------------------------------------------------------
 
+Running /home/ubuntu/workspace/lab44/lab44.cpp
+Please enter your first name (Up to 10 Letters): James
+Please enter your last name (Up to 20 Letters): James
+Your last name and first name are the same, please restart the program if this is an error.
+Please enter your first name (Up to 10 Letters): James
+
+Please enter your last name (Up to 20 Letters): James
+
+Which of the followuing user names do you like?
+
+JamesJames
+JJames
+JamesJ
 
 */
